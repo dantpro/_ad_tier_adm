e5a7bda4eb40d8084a1bff9c812a49836dd73153
Dmitry Antonov <dant@dant.pro>
2025-01-28 13:35:32 +0700

    something was changed

--- 00-Create-All.ps1
+++ 00-Create-All.ps1
@@ -7,7 +7,7 @@
 & "$PSScriptRoot\04-Create-PSOs.ps1"
 & "$PSScriptRoot\05-Create-GPOs.ps1 $PSScriptRoot\gpo-list.csv"
 #& "$PSScriptRoot\06-Create-ACEs.ps1"
-& "$PSScriptRoot\06-Create-ACEs.ps1 -LIVE"
+& "$PSScriptRoot\06-Create-ACEs.ps1" LIVE
 & "$PSScriptRoot\07-Create-Users.ps1"
 
 
